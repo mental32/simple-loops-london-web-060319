c3a25257382ee2552840fa6f1f7667a8bbc590a4
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-28 09:19:56 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -61,7 +61,7 @@ def for_iterator(number_of_times)
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
 
-  for _ in (0..number_of_times) do
+  for _ in (1..number_of_times) do
     puts phrase
   end
 end
